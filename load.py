4933d8def7bab7a38a176dd887e48018392783ed
Wormings <zhang_shun97@126.com>
2018-01-27 11:15:41 +0800

    zs

--- load.py
+++ load.py
@@ -4,16 +4,34 @@ import torch
 from torchvision import transforms, datasets
 import numpy as np
 import os
+import argparse
 
 os.chdir('./data/')
 
-
-class Args:
-
-    def __init__(self):
-        self.data = os.getcwd()
-        self.batchSize = 0
-        self.nThreads = 1
+# arguments and initializations
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+parser.add_argument('--data', metavar='PATH', required=True,
+                    help='path to dataset')
+parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet18',
+                    help='model architecture: resnet18 | resnet34 | ...'
+                         '(default: resnet18)')
+parser.add_argument('--gen', default='gen', metavar='PATH',
+                    help='path to save generated files (default: gen)')
+parser.add_argument('--nThreads', '-j', default=2, type=int, metavar='N',
+                    help='number of data loading threads (default: 2)')
+parser.add_argument('--nEpochs', default=90, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--epochNumber', default=1, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('--batchSize', '-b', default=256, type=int, metavar='N',
+                    help='mini-batch size (1 = pure stochastic) Default: 256')
+parser.add_argument('--lr', default=0.1, type=float, metavar='LR',
+                    help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--weightDecay', default=1e-4, type=float, metavar='W',
+                    help='weight decay')
+args = parser.parse_args()
 
 
 # Data loading code
@@ -25,7 +43,6 @@ transform = transforms.Compose([
                          std=[0.229, 0.224, 0.225]),
 ])
 
-args = Args()
 traindir = os.path.join(args.data, 'train')
 valdir = os.path.join(args.data, 'val')
 train = datasets.ImageFolder(traindir, transform)
